c76b19a5d040691befa2f1577fc9ce00eeb92304
장효원 <hyo961015@naver.com>
2021-12-28 13:21:39 +0900

    init

---
+++ src/main/java/com/janghyo/springbootjsonexample/SpringBootJsonExampleApplication.java
@@ -0,0 +1,13 @@
+package com.janghyo.springbootjsonexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJsonExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsonExampleApplication.class, args);
+    }
+
+}
